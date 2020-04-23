ecfbb35da7e30b52cf30410b8c71ebe59bb10638
Antonio Espinoza <aespinoza777@gmail.com>
2020-04-23 01:11:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(phrase)
   statement = []
   phrase.each do |word|
-    statement << "Hello there, and how are you?"
+    word.reverse "Hello there, and how are you?"
   end
   statement
 end
